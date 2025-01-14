e02d5611b768adb6d4997bbd26d2716f15b63332
Nikithavedanth <111447690+Nikithavedanth@users.noreply.github.com>
2025-01-14 18:14:43 -0500

    Updated

--- AngularFramework/Nikki-workspace/projects/event-binding/src/app/app.component.ts
+++ AngularFramework/Nikki-workspace/projects/event-binding/src/app/app.component.ts
@@ -4,12 +4,12 @@ import { EventBindingComponent } from './event-binding/event-binding.component';
 import { KeyEventsComponent } from './key-events/key-events.component';
 import { PipesDemoComponent } from './pipes-demo/pipes-demo.component';
 import { CustomDirComponent } from './custom-dir/custom-dir.component';
-
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CustomDirComponent],
+  imports: [RouterOutlet,LoginPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ AngularFramework/Nikki-workspace/projects/event-binding/src/app/login-page/login-page.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-page',
+  standalone: true,
+  imports: [CommonModule,ReactiveFormsModule],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.css'
+})
+export class LoginPageComponent {
+  imagePath: string = 'assets/AWS.png';
+}
